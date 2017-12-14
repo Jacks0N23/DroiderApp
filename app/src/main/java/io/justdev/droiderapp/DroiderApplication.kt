9671bc204561b0dc5d9035f701ed648628d44745
a.guzenko <a.guzenko@tinkoff.ru>
2017-12-14 14:24:18 +0300

    Initial commit

---
+++ app/src/main/java/io/justdev/droiderapp/DroiderApplication.kt
@@ -0,0 +1,12 @@
+package io.justdev.droiderapp
+
+import android.app.Application
+
+
+class DroiderApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //TODO init dagger here
+    }
+}
